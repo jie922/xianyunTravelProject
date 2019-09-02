be14a681a9048e680f0aeff70ebd9332de148052
蔡木界 <1663744776@qq.com>
2019-09-02 15:36:37 +0800

    02公共组件页头和页脚及轮播图部分

---
+++ components/header.vue
@@ -0,0 +1,63 @@
+<template>
+  <header class="header">
+    <el-row type="flex" justify="space-between" class="main">
+      <div class="logo">
+        <!-- logo -->
+        <nuxt-link to="/">
+          <img src="@/images/logo.jpg" alt />
+        </nuxt-link>
+      </div>
+      <!-- 菜单栏 -->
+      <el-row type="flex" class="navs">
+        <nuxt-link to="/">首页</nuxt-link>
+        <nuxt-link to="/post/post_index">旅游攻略</nuxt-link>
+        <nuxt-link to="/hotel/hotel_index">酒店</nuxt-link>
+        <nuxt-link to="/air/air_index">国内机票</nuxt-link>
+      </el-row>
+      <!-- 用户登录注册 -->
+      <el-row type="flex">
+        <nuxt-link to="/">消息</nuxt-link>
+        <nuxt-link to="/">登录/注册</nuxt-link>
+      </el-row>
+    </el-row>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.header {
+  height: 60px;
+  line-height: 60px;
+  .main {
+    width: 1000px;
+    margin: 0 auto;
+  }
+  .logo {
+    width: 156px;
+    padding-top: 8px;
+    img {
+      display: block;
+      width: 100%;
+    }
+  }
+  .navs {
+    flex: 1;
+    a {
+      color: #000;
+      padding: 0 20px; 
+      &:hover{
+        color:#03d5fa;
+        border-bottom: 5px #03d5fa solid;
+      }
+    }
+    .nuxt-link-exact-active{
+      color:#fff;
+      background-color:#1423f7;
+      
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,39 @@
 <template>
-  <div>首页</div>
+  <div class="shouye">
+    <!-- 页面轮播图 -->
+    <el-carousel height="700px">
+      <el-carousel-item v-for="(item, index) in imager " :key="index">
+        <div :style="`background:url(${$axios.defaults.baseURL+item.url}) center center no-repeat`" class="image"></div>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      imager: []
+    };
+  },
+  mounted() {
+    this.$axios({
+      url: "/scenics/banners"
+    }).then(res => {
+      console.log(res);
+      this.imager = res.data.data;
+    });
+    console.dir(this.$axios);
+  }
+};
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.shouye {
+  .image {
+    height:100%;
+    // 自适应宽高
+    background-size:contain contain;
+  }
+}
 </style>
