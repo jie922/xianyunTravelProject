7cb1784711d5c5e7786ec7adbcebef6848df223b
蔡木界 <1663744776@qq.com>
2019-09-03 10:36:07 +0800

    03Tab栏搜索框功能实现

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,43 @@
 <template>
   <div>
-    <Header/>
+    <Header />
     <nuxt />
-    <Footer/>
+    <Footer />
   </div>
 </template>
 <script>
-import Header from '@/components/header.vue'
-import Footer from '@/components/footer.vue'
+import Header from "@/components/header.vue";
+import Footer from "@/components/footer.vue";
 export default {
-   components:{
-      Header,
-      Footer
-    }
-  
-}
+  components: {
+    Header,
+    Footer
+  }
+};
 </script>
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
 
+ul,
+li,
+ol {
+  list-style: none;
+}
+
+i,
+em {
+  font-style: normal;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+a:hover {
+  color: inherit;
+}
 </style>
