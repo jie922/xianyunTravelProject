48a097a930b47a750d470c065876f3bb59f15bb3
蔡木界 <1663744776@qq.com>
2019-09-04 20:41:03 +0800

    10注册表单数据绑定

--- components/user/enrollForm.vue
+++ components/user/enrollForm.vue
@@ -2,11 +2,11 @@
   <el-form class="form">
 
     <el-form-item class="form-item">
-      <el-input placeholder="用户名手机"></el-input>
+      <el-input placeholder="用户名手机" v-model="form.username"></el-input>
     </el-form-item>
 
     <el-form-item class="form-item">
-      <el-input placeholder="验证码">
+      <el-input placeholder="验证码" v-model="form.captcha">
         <template>
           <el-button slot="append">发送验证码</el-button>
         </template>
@@ -14,22 +14,37 @@
     </el-form-item>
 
     <el-form-item class="form-item">
-      <el-input placeholder="你的名字"></el-input>
+      <el-input placeholder="你的名字" v-model="form.nickname"></el-input>
     </el-form-item>
 
     <el-form-item class="form-item">
-      <el-input placeholder="密码"></el-input>
+      <el-input placeholder="密码" v-model="form.password"></el-input>
     </el-form-item>
 
     <el-form-item class="form-item">
-      <el-input placeholder="确认密码"></el-input>
+      <el-input placeholder="确认密码" v-model="form.checkPassword"></el-input>
     </el-form-item>
     <el-button type="primary" class="submit">注册</el-button>
   </el-form>
 </template>
 
 <script>
-export default {};
+export default {
+  
+  data(){
+    return {
+    // 表单数据绑定
+      form:{
+           username: "",   // 登录用户名/手机
+            password: "" ,   // 登录密码
+            checkPassword: "", // 确认密码
+            nickname: "",	// 昵称
+            captcha: ""		// 手机验证码
+      }
+    }
+    
+  }
+};
 </script>
 
 <style lang='less' scoped>
